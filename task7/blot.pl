b54b79d2ccc08b2e7f7c317924031e50a8eb328a
Mary Mitienko <mitienko.m@yandex.ru>
2016-09-30 23:52:48 +0300

    Update blot.pl

--- task7/blot.pl
+++ task7/blot.pl
@@ -1,7 +1,10 @@
-a(X) :- not(X=victor), X=borya.
-b(X) :- X=victor, not(X=alex).
-v(X) :- not(X=borya), not(not(X=victor)).
-soil(X) :- (a(X), b(X), (X=borya), not(X=victor)).
-soil(X) :- (a(X), not(X=victor), X=alex, v(X)).
-soil(X) :- (X=victor, not(X=borya), b(X), v(X)).
+a(X) :- not(X=victor).
+al(X) :- X=borya.
+b(X) :- X=victor.
+br(X) :- not(X=alex).
+v(X) :- not(X=borya).
+vc(X) :- not(not(X=victor)).
+soil(X) :- (a(X), al(X), b(X), br(X), not(v(X)), not(vc(X))).
+soil(X) :- (a(X), al(X), not(b(X)), not(br(X)), v(X), vc(X)).
+soil(X) :- (not(a(X)), not(al(X)), b(X), br(X), v(X), vc(X)).
                
