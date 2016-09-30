ec4061543663fbd5cb237a4f1046b2b7c11efffd
Mary Mitienko <mitienko.m@yandex.ru>
2016-09-30 23:00:41 +0300

    Update blot.pl

--- task7/blot.pl
+++ task7/blot.pl
@@ -1,7 +1,7 @@
 a(X) :- not(X=victor), X=borya.
 b(X) :- X=victor, not(X=alex).
 v(X) :- not(X=borya), ((X=victor);not(X=victor)).
-запачкал(X) :- (a(X), b(X), not(v(X))).
-запачкал(X) :- (a(X), not(b(X)), v(X)).
-запачкал(X) :- (not(a(X)), b(X), v(X)).
+soil(X) :- (a(X), b(X), not(v(X))).
+soil(X) :- (a(X), not(b(X)), v(X)).
+soil(X) :- (not(a(X)), b(X), v(X)).
                
