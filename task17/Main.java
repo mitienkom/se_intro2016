9c3c78db496dd04c8d1ef9c9837a3d80d470bd60
Mary Mitienko <mitienkom@users.noreply.github.com>
2016-11-11 19:27:05 +0300

    Update Main.java

--- task17/Main.java
+++ task17/Main.java
@@ -1,16 +1,18 @@
 import java.util.Scanner; 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        boolean b = true;
-        for (int i = 0; i < s.length() / 2; ++i) {
-        	if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-        		b = false;
-        		break;
-        	}
-        }
-        if (b) System.out.println("True");
-        else System.out.println("False");      
-    }
+public class Main { 
+public static boolean fun(String s) { 
+for (int i = 0; i < s.length() / 2; ++i) { 
+if (s.charAt(i) != s.charAt(s.length() - 1 - i)) { 
+return false; 
+} 
+} 
+return true; 
+} 
+
+public static void main(String[] args) { 
+Scanner sc = new Scanner(System.in); 
+String s = sc.nextLine(); 
+System.out.println(fun(s)); 
+} 
+
 }
