7ff3cee0b65654899ce43e1cc3c2f38849b67fd2
Mary Mitienko <mitienkom@users.noreply.github.com>
2016-11-20 23:10:17 +0300

    Update Main.java

--- task17/Main.java
+++ task17/Main.java
@@ -1,18 +1,15 @@
-import java.util.Scanner; 
+
 public class Main { 
+    public static void main(String[] args) { 
+        String s= "АРОЗАУПАЛАНАЛАПУАЗОРА"; 
+        System.out.println(fun(s)); 
+        } 
+    }
 public static boolean fun(String s) { 
-for (int i = 0; i < s.length() / 2; ++i) { 
-if (s.charAt(i) != s.charAt(s.length() - 1 - i)) { 
-return false; 
+    for (int i = 0; i < s.length() / 2; ++i) { 
+        if (s.charAt(i) != s.charAt(s.length() - 1 - i)) { 
+            return false; 
+            } 
+        } 
+    return true; 
 } 
-} 
-return true; 
-} 
-
-public static void main(String[] args) { 
-Scanner sc = new Scanner(System.in); 
-String s = sc.nextLine(); 
-System.out.println(fun(s)); 
-} 
-
-}
